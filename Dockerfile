a25221ac9dd33b93cf525ff3f6c83d45b7a852a2
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-01-04 13:12:37 +0100

    edit dockerfile for dbconnection

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM openjdk:8-jdk-alpine
 
-ENV LAS2PEER_PORT=9011
+ENV LAS2PEER_PORT=9014
+ENV DATABASE_NAME=SBF
+ENV DATABASE_HOST=mobsos-mysql.mobsos
+ENV DATABASE_PORT=3306
+ENV DATABASE_USER=root
+ENV DATABASE_PASSWORD=root
 
 RUN apk add --update bash mysql-client apache-ant && rm -f /var/cache/apk/*
 RUN addgroup -g 1000 -S las2peer && \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,7 @@ export SERVICE_VERSION=$(awk -F "=" '/service.version/ {print $2}' etc/ant_confi
 export SERVICE_NAME=$(awk -F "=" '/service.name/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE_CLASS=$(awk -F "=" '/service.class/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE=${SERVICE_NAME}.${SERVICE_CLASS}@${SERVICE_VERSION}
+export CREATE_DB_SQL='initDB.sql'
 
 # wait for any bootstrap host to be available
 if [[ ! -z "${BOOTSTRAP}" ]]; then
@@ -25,6 +26,29 @@ if [[ ! -z "${BOOTSTRAP}" ]]; then
             break
         fi
     done
+fi
+
+function set_in_service_config {
+    sed -i "s?${1}[[:blank:]]*=.*?${1}=${2}?g" ${SERVICE_PROPERTY_FILE}
+}
+cp $SERVICE_PROPERTY_FILE.sample $SERVICE_PROPERTY_FILE
+set_in_service_config databaseName ${DATABASE_NAME}
+set_in_service_config databaseHost ${DATABASE_HOST}
+set_in_service_config databasePort ${DATABASE_PORT}
+set_in_service_config databaseUser ${DATABASE_USER}
+set_in_service_config databasePassword ${DATABASE_PASSWORD}
+
+# ensure the database is ready
+while ! mysqladmin ping -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} --silent; do
+    echo "Waiting for mysql at ${DATABASE_HOST}:${DATABASE_PORT}..."
+    sleep 1
+done
+echo "${DATABASE_HOST}:${DATABASE_PORT} is available. Continuing..."
+
+# Create and migrate the database on first run
+if ! mysql -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} -e "desc ${DATABASE_NAME}.MESSAGE" > /dev/null 2>&1; then
+    echo "Creating database schema..."
+    mysql -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} ${DATABASE_NAME} < ${CREATE_DB_SQL}
 fi
 
 # prevent glob expansion in lib/*
