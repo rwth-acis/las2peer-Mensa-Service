92ed8332c2195c9376c1f26cdb670c4526113e8f
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-01-05 11:46:52 +0100

    modified dockerfile to work with kubernetes

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,20 @@
 FROM openjdk:8-jdk-alpine
 
 ENV LAS2PEER_PORT=9014
-ENV DATABASE_NAME=SBF
+ENV DATABASE_NAME=LAS2PEERMON
 ENV DATABASE_HOST=mobsos-mysql.mobsos
 ENV DATABASE_PORT=3306
 ENV DATABASE_USER=root
 ENV DATABASE_PASSWORD=root
 
-RUN apk add --update bash mysql-client apache-ant && rm -f /var/cache/apk/*
+RUN apk add --update bash mysql-client apache-ant curl && rm -f /var/cache/apk/*
 RUN addgroup -g 1000 -S las2peer && \
     adduser -u 1000 -S las2peer -G las2peer
 
 COPY --chown=las2peer:las2peer . /src
 WORKDIR /src
 
+RUN chmod -R a+rwx /src
 RUN chmod +x /src/docker-entrypoint.sh
 RUN dos2unix docker-entrypoint.sh
 # run the rest as unprivileged user
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,38 @@ if [[ ! -z "${DEBUG}" ]]; then
 fi
 
 # set some helpful variables
+export SERVICE_PROPERTY_FILE='etc/i5.las2peer.services.mensaSrvice.MensaService.properties'
 export SERVICE_VERSION=$(awk -F "=" '/service.version/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE_NAME=$(awk -F "=" '/service.name/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE_CLASS=$(awk -F "=" '/service.class/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE=${SERVICE_NAME}.${SERVICE_CLASS}@${SERVICE_VERSION}
 export CREATE_DB_SQL='initDB.sql'
+
+function set_in_service_config {
+    sed -i "s?${1}[[:blank:]]*=.*?${1}=${2}?g" ${SERVICE_PROPERTY_FILE}
+}
+cp $SERVICE_PROPERTY_FILE.sample $SERVICE_PROPERTY_FILE
+set_in_service_config databaseName ${DATABASE_NAME}
+set_in_service_config databaseHost ${DATABASE_HOST}
+set_in_service_config databasePort ${DATABASE_PORT}
+set_in_service_config databaseUser ${DATABASE_USER}
+set_in_service_config databasePassword ${DATABASE_PASSWORD}
+
+
+
+
+# ensure the database is ready
+while ! mysqladmin ping -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} --silent; do
+    echo "Waiting for mysql at ${DATABASE_HOST}:${DATABASE_PORT}..."
+    sleep 1
+done
+echo "${DATABASE_HOST}:${DATABASE_PORT} is available. Continuing..."
+
+# Create and migrate the database on first run
+if ! mysql -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} -e "desc ${DATABASE_NAME}.MESSAGE" > /dev/null 2>&1; then
+    echo "Creating database schema..."
+    mysql -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} ${DATABASE_NAME} < ${CREATE_DB_SQL}
+fi
 
 # wait for any bootstrap host to be available
 if [[ ! -z "${BOOTSTRAP}" ]]; then
@@ -28,28 +55,6 @@ if [[ ! -z "${BOOTSTRAP}" ]]; then
     done
 fi
 
-function set_in_service_config {
-    sed -i "s?${1}[[:blank:]]*=.*?${1}=${2}?g" ${SERVICE_PROPERTY_FILE}
-}
-cp $SERVICE_PROPERTY_FILE.sample $SERVICE_PROPERTY_FILE
-set_in_service_config databaseName ${DATABASE_NAME}
-set_in_service_config databaseHost ${DATABASE_HOST}
-set_in_service_config databasePort ${DATABASE_PORT}
-set_in_service_config databaseUser ${DATABASE_USER}
-set_in_service_config databasePassword ${DATABASE_PASSWORD}
-
-# ensure the database is ready
-while ! mysqladmin ping -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} --silent; do
-    echo "Waiting for mysql at ${DATABASE_HOST}:${DATABASE_PORT}..."
-    sleep 1
-done
-echo "${DATABASE_HOST}:${DATABASE_PORT} is available. Continuing..."
-
-# Create and migrate the database on first run
-if ! mysql -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} -e "desc ${DATABASE_NAME}.MESSAGE" > /dev/null 2>&1; then
-    echo "Creating database schema..."
-    mysql -h${DATABASE_HOST} -P${DATABASE_PORT} -u${DATABASE_USER} -p${DATABASE_PASSWORD} ${DATABASE_NAME} < ${CREATE_DB_SQL}
-fi
 
 # prevent glob expansion in lib/*
 set -f
@@ -57,6 +62,10 @@ LAUNCH_COMMAND='java -cp lib/* i5.las2peer.tools.L2pNodeLauncher -s service -p '
 if [[ ! -z "${BOOTSTRAP}" ]]; then
     LAUNCH_COMMAND="${LAUNCH_COMMAND} -b ${BOOTSTRAP}"
 fi
+
+
+#prepare pastry properties
+echo external_address = $(curl -s https://ipinfo.io/ip):${LAS2PEER_PORT} > etc/pastry.properties
 
 # start the service within a las2peer node
 if [[ -z "${@}" ]]
